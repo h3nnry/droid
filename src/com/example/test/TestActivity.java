981b0396d384a6ca93ecd8ba93765ed2d4e55803
h3nry <h3nnry84@gmail.com>
2014-08-09 22:44:02 +0300

    first application for android

---
+++ src/com/example/test/TestActivity.java
@@ -0,0 +1,108 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bignerdranch.android.test.TrueFalse;
+
+public class TestActivity extends ActionBarActivity {
+	private Button mTrueButton;
+	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[]{
+			new TrueFalse(R.string.question_suma, true),
+			new TrueFalse(R.string.question_grup, true),
+			new TrueFalse(R.string.question_zi, false),
+			new TrueFalse(R.string.question_mileniu, true),
+			new TrueFalse(R.string.question_culori, false),
+	};
+	private int mCurrentIndex = 0;
+	
+	private void updateQuestion(){
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+	}
+	private void checkAnswer(boolean userPressedTrue){
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		
+		int messageResId = 0;
+		
+		if (userPressedTrue == answerIsTrue){
+			messageResId = R.string.correct_toast;
+		}
+		else{
+			messageResId = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this,  messageResId, Toast.LENGTH_SHORT)
+			.show();
+	}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+		
+		mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText(question);
+		
+		mTrueButton = (Button)findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkAnswer(true);
+				
+			}
+		});
+		mFalseButton = (Button)findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkAnswer(false);
+				
+			}
+		});
+		mNextButton = (Button)findViewById(R.id.next_button);
+		mNextButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+		updateQuestion();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.test, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
